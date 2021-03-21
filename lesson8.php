d5d05dec172007c1d43431e144a114799774dc02
DmitriLeon2000 <dmitryaveron2000@gmail.com>
2021-03-21 23:30:56 +0900

    Add files via upload

---
+++ lesson8.php
@@ -0,0 +1,10 @@
+<!-- [WORK IN PROGRESS] -->
+<h3>Вариант 7</h3>
+<p>Дан массив размера N и целые числа K и L (1 ≤ K ≤ L ≤ N). Найти среднее арифметическое элементов массива с номерами от K до L включительно.<br/>
+</p>
+<form action="" method="POST">
+	<label>N = </label><input name='n' type='number' value='0'/><br/>
+	<input type='submit' value='⇓⇓⇓⇓⇓'/>
+	<br/>
+	<br/>
+</form>
