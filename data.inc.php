7e3d8a2cf0e365fb701503da283fe5915551cbe6
DmitriLeon2000 <dmitryaveron2000@gmail.com>
2021-03-21 23:28:57 +0900

    Add files via upload

--- data.inc.php
+++ data.inc.php
@@ -13,6 +13,7 @@
 		['link'=>'Задание №4', 'href'=>'index.php?id=lesson4'],
 		['link'=>'Задание №5', 'href'=>'index.php?id=lesson5'],
 		['link'=>'Задание №6', 'href'=>'index.php?id=lesson6'],
+		['link'=>'Задание №7', 'href'=>'index.php?id=lesson7'],
 		['link'=>'Контакты', 'href'=>'index.php?id=contact']
 		];
 	$day = strftime('%d');
