8b2619b964c8f16a421eb5ef566f05a9d2df6229
DmitriLeon2000 <dmitryaveron2000@gmail.com>
2021-03-29 00:13:35 +0900

    UPDATE

--- lesson8.php
+++ lesson8.php
@@ -3,19 +3,34 @@
 <p>Дан массив размера N и целые числа K и L (1 ≤ K ≤ L ≤ N). Найти среднее арифметическое элементов массива с номерами от K до L включительно.<br/>
 </p>
 <form action="<?= $_SERVER['REQUEST_URI']?>" method="POST">
-	<label>N = </label><input name='n' type='number' value='<?= (isset($_POST['n'])) ? $_POST['n'] : 1?>' min=1/><br/>
+	<? (isset($_POST['n'])) ? $n=$_POST['n'] : $n=0;?>
+	<label>N = </label><input name='n' type='number' value='<?=$n?>' min=1/><br/>
 	<input type='submit' value='Задать массив'/><br/>
-</form>
-<form action="<?= $_SERVER['REQUEST_URI']?>" method="POST">
 	<?
-		for ($i=1; $i <= $_POST['n']; $i++) {
-			echo "<input name='arr[]' type='number' value=0 style='width: 32px;'/>";
+	(isset($_POST['k'])) ? $k=$_POST['k'] : $k=1;
+	(isset($_POST['l'])) ? $l=$_POST['l'] : $l=1;
+	if ($n >= 1) {
+		for ($i=1; $i <= $n; $i++) {
+			echo "<input name='arr[]' type='number' value='";
+			echo (isset($_POST['arr'][$i-1])) ? $_POST['arr'][$i-1] : 0;
+			echo "' style='width: 32px;'/>";
 		}
-		if (isset($_POST['n'])) {
-			echo "<label>K = </label><input name='k' type='number' value='<?= (isset($_POST['k'])) ? $_POST['k'] : 1?>' min=1/><br/>";
-			echo "<label>L = </label><input name='l' type='number' value='<?= (isset($_POST['l'])) ? $_POST['l'] : 1?>' min=1/><br/>";
-			echo "<input type='submit' value='⇓⇓⇓⇓⇓'/>";
+		echo "<br/>";
+		echo "<label>K = </label>";
+		echo "<input name='k' type='number' value='$k' min='1' max='$n'/><br/>";
+		echo "<label>L = </label>";
+		echo "<input name='l' type='number' value='$l' min='1' max='$n'/><br/>";
+		echo "<input type='submit' value='⇓⇓⇓⇓⇓'/>";
+		echo "<br/>";
+		$sum = 0;
+		if ($k <= $l) {
+		for ($i=($k-1); $i <= ($l-1); $i++) {
+			$sum = $sum + $_POST['arr'][$i];
 		}
-	?>
+		$avg = $sum/($l-$k+1);
+		echo '<span style="text-decoration:overline">N</span><sub>K</sub><sup>L</sup> = '.$avg;
+		}
+		else echo 'ОШИБКА: значение K должно быть меньше или равно L.';
+	} ?>
 	<br/>
 </form>
